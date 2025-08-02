cda375a6385c08e57264ed4af00f4b9a27862375
Som Sinha <somdsinha@gmail.com>
2025-08-03 00:31:22 +0100

    fix: remove redundant error types

--- s3-proxy-rs/src/errors.rs
+++ s3-proxy-rs/src/errors.rs
@@ -4,10 +4,6 @@ use thiserror::Error;
 pub enum Error {
     #[error("Connection error: {0}")]
     ConnectionError(#[from] std::io::Error),
-    #[error("Not found: {0}")]
-    NotFound(String),
-    #[error("S3 connection error")]
-    S3Error(String),
     #[error("Internal error: {0}")]
     InternalError(String),
 }
